a6fbd7007b61740f78c1350527cdad392d1e34d4
Julio Cesar <jcmonzem@hotmail.com>
2025-12-24 18:05:47 -0300

    UI improvements for parent LWC

--- force-app/main/default/lwc/parentLWC/parentLWC.js
+++ force-app/main/default/lwc/parentLWC/parentLWC.js
@@ -1,23 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
 
-    _resource = {
-        name: null
+    @track _resource = {
+        resourceId: null
     };
+
+    accountName;
+    accountId;
+
+    get hasAccSelected() {
+        return !!this.accountId;
+    }
 
     handleUpdate(event) {
         const selectedRecord = event.detail.selectedRecord;
-
         if (selectedRecord) {
-            this._resource.name = selectedRecord.Id;
+            this._resource.Id = selectedRecord.Id;
+            this.accountId = selectedRecord.Id;
+            this.accountName = selectedRecord.Name;
         } else {
-            this._resource.name = null;
+            this._resource.Id = null;
+            this.accountId = null;
+            this.accountName = null;
         }
-
-        console.log(
-            'Name: ',
-            this._resource.name
-        );
     }
 }
